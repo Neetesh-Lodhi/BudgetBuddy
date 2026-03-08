64549e06271cabc3b7b24997de10767f0d690241
neetesh <neeteshlodhi9555@email.com>
2026-03-08 19:10:32 +0530

    Added Vercel.json for deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://inventorybuddy.onrender.com/api",
 });
 
 // Attach JWT token properly
